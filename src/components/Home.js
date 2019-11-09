35f4856e49783ccb47e3150827c17f698fab4ec5
Charikleia Charalampidou <cl.charal@gmail.com>
2019-11-09 13:23:18 +0000

    Adding Education section

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,21 @@ export const Home = () => {
     <section id="home" className="bg-light d-flex flex-fill py-5">
       <div className="container d-flex justify-content-center align-items-center">
         <div className="row d-flex justify-content-center">
-          <div className="col d-flex flex-column justify-content-center align-items-center">
-            <h1 className="heading">Hello, I'm Claire.</h1>
-            <h2>I'm a Developer.</h2>
+          <div className="col d-flex flex-column justify-content-center">
+            <h1 className="heading text-center">Hello, I'm Claire.</h1>
+            <h2 className="text-center">
+              Junior Developer &#64; <br />
+              <a
+                className="company-name"
+                href="https://signal-noise.co.uk"
+                target="_blank"
+              >
+                Signal Noise, part of the Economist Group
+              </a>
+              .
+            </h2>
           </div>
-          <div className="col d-flex justify-content-center align-items-center">
+          <div className="col-5 d-flex justify-content-center">
             <img
               className="rounded-circle w-50"
               src="53490573.jpeg"
