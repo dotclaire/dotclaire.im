c715e71e4b28f4465fa6932d227751d17a0fc629
Charikleia Charalampidou <cl.charal@gmail.com>
2020-06-13 22:26:43 +0100

    Remove jquery and popper.js packages

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,1 @@
-import * as jQuery from "jquery"
-import Popper from "popper.js"
 import "./src/styles/global.css"
-
-window.$ = jQuery
-window.Popper = Popper
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,53 +1,49 @@
 import React from "react"
 import { Logo } from "../icons/Logo"
-import "popper.js"
-import "jquery/dist/jquery.slim"
 
-export class Navbar extends React.Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light navbar-expand-lg bg-light fixed-top">
-        <div className="container">
-          <a className="navbar-brand" href="/">
-            <Logo className="logo" />
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div id="navbarSupportedContent" className="navbar-collapse collapse">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <a className="nav-link h5 text-dark" href="#home">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link h5 text-dark" href="#aboutme">
-                  About
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link h5 text-dark" href="#education">
-                  Education
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link h5 text-dark" href="#skills">
-                  Skills
-                </a>
-              </li>
-            </ul>
-          </div>
+export const Navbar = () => {
+  return (
+    <nav className="navbar navbar-light navbar-expand-lg bg-light fixed-top">
+      <div className="container">
+        <a className="navbar-brand" href="/">
+          <Logo className="logo" />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div id="navbarSupportedContent" className="navbar-collapse collapse">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link h5 text-dark" href="#home">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link h5 text-dark" href="#aboutme">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link h5 text-dark" href="#education">
+                Education
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link h5 text-dark" href="#skills">
+                Skills
+              </a>
+            </li>
+          </ul>
         </div>
-      </nav>
-    )
-  }
+      </div>
+    </nav>
+  )
 }
