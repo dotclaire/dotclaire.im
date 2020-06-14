45eddc6b3e4d0023a3bd01e4e5a2936680a5024a
Charikleia Charalampidou <cl.charal@gmail.com>
2020-06-14 14:14:46 +0100

    Add about section

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,9 +5,25 @@ import { Footer } from "..//components/Footer"
 export default class About extends React.Component {
   render() {
     return (
-      <div className="bg-grey h-full">
+      <div className="bg-grey">
         <Navbar />
-        <h1>About</h1>
+        <div className="container mx-auto py-5 flex justify-between">
+          <div>
+            <img
+              className="rounded-full"
+              src="https://avatars3.githubusercontent.com/u/53490573?v=4"
+            />
+          </div>
+          <div className="flex flex-col justify-start max-w-lg">
+            <h1>About</h1>
+            <p>
+              My name is Charikleia Charalampidou, but people call me Claire! I
+              am a Front End Developer with passion for responsive web design
+              and a firm believer in the mobile first approach. I love getting
+              my hands on new technologies and building contemporary projects.
+            </p>
+          </div>
+        </div>
         <Footer />
       </div>
     )
