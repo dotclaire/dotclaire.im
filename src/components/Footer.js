ab8b701a22102dd7f07f7e1bdca8386b359aab3b
Charikleia Charalampidou <cl.charal@gmail.com>
2020-06-14 10:57:43 +0100

    Add icons on footer and fix navbar styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,10 @@
 import React from "react"
-import { Github } from "../icons/Github"
-import { Linkedin } from "../icons/Linkedin"
-import { Twitter } from "../icons/Twitter"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faGithub,
+  faTwitter,
+  faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons"
 
 export class Footer extends React.Component {
   render() {
@@ -9,13 +12,13 @@ export class Footer extends React.Component {
       <footer className="flex flex-col justify-center items-center py-5">
         <div className="flex flex-row">
           <a href="https://www.github.com/clchar" className="pr-3">
-            <Github />
+            <FontAwesomeIcon icon={faGithub} fixedWidth />
           </a>
           <a href="https://www.linkedin.com/in/clchar" className="pr-3">
-            <Linkedin />
+            <FontAwesomeIcon icon={faLinkedinIn} fixedWidth />
           </a>
           <a href="https://www.twitter.com/CCharalampidou" className="pr-3">
-            <Twitter />
+            <FontAwesomeIcon icon={faTwitter} fixedWidth />
           </a>
         </div>
         <div className="text-white">&copy; 2019 Claire Charalampidou</div>
