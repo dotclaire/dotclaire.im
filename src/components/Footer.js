52dd7c137df65d4c7b8217299084ac7217fd3e29
Charikleia Charalampidou <cl.charal@gmail.com>
2020-06-21 15:31:27 +0100

    Replace 2019 with current year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,15 @@
-import React from "react"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {
   faGithub,
-  faTwitter,
   faLinkedinIn,
+  faTwitter,
 } from "@fortawesome/free-brands-svg-icons"
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import React from "react"
+
 export const Footer = ({ isDark }) => {
+  const date = new Date()
+  const year = date.getFullYear()
   return (
     <div className={`${isDark ? "bg-none" : "bg-primary"}`}>
       <footer className="flex flex-col justify-center items-center py-5">
@@ -21,7 +24,7 @@ export const Footer = ({ isDark }) => {
             <FontAwesomeIcon icon={faTwitter} fixedWidth />
           </a>
         </div>
-        <div className="text-white">&copy; 2019 Claire Charalampidou</div>
+        <div className="text-white">&copy; {year} Claire Charalampidou</div>
       </footer>
     </div>
   )
