58455581f047d08511d5e0f519a796877e0fd855
Charikleia Charalampidou <cl.charal@gmail.com>
2020-06-28 12:39:26 +0100

    Fix text colors in about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,14 +10,14 @@ export default class About extends React.Component {
         <div className="container mx-auto max-w-container p-4 md:py-10 md:p-5">
           <div className="flex flex-col justify-around md:flex-row">
             <div className="flex flex-col justify-start mt-5 order-2 flex-1 md:mr-5 md:mt-0 md:order-1">
-              <p className="text-2xl text-justify text-black-text">
+              <p className="text-2xl text-justify text-main-text">
                 My name is Charikleia Charalampidou, but people call me Claire!
                 I am a Front End Developer with passion for responsive web
                 design and a firm believer in the mobile first approach. I love
                 getting my hands on new technologies and building contemporary
                 projects.
               </p>
-              <p className="text-grey-text text-xl py-5 text-justify">
+              <p className="text-secondary-text text-xl py-5 text-justify">
                 Before I got into software, I did my undergraduate studies in
                 Mathematics and got my Master's degree in Data Analytics. While
                 studying full time, I was growing my skills as a developer.
@@ -33,18 +33,18 @@ export default class About extends React.Component {
           </div>
           <div className="flex flex-col justify-around py-5 md:flex-row">
             <div className="flex flex-col justify-start flex-1 md:mr-5">
-              <h2 className="text-2xl text-black-text">Education</h2>
-              <hr className="block h-line bg-secondary w-32" />
-              <h3 className="pt-4 text-xl text-black-text">
+              <h2 className="text-2xl text-main-text">Education</h2>
+              <hr className="block h-line bg-green w-32" />
+              <h3 className="pt-4 text-xl text-main-text">
                 Masters of Science in Data Analytics (September 2018 - October
                 2019)
               </h3>
-              <h2 className="pb-4 text-black-text">
+              <h2 className="pb-4 text-main-text">
                 University of Brighton, United Kingdom
                 <br />
                 School of Computing, Engineering and Mathematics
               </h2>
-              <p className="text-grey-text text-xl text-justify">
+              <p className="text-secondary-text text-xl text-justify">
                 The MSc course in Data Analytics covered three main areas; Data
                 Management, Data Interpretation and Project Management. I had
                 the opportunity to attend modules such as Data Management (in
@@ -57,14 +57,14 @@ export default class About extends React.Component {
                 algorithms, implementing them in RStudio.
               </p>
 
-              <h3 className="pt-4 text-xl text-black-text">
+              <h3 className="pt-4 text-xl text-main-text">
                 Bachelors of Science in Mathematics (September 2011 - June 2017)
               </h3>
-              <h2 className="pb-4 text-black-text">
+              <h2 className="pb-4 text-main-text">
                 Aristotle University of Thessaloniki, Greece <br />
                 Faculty of Physics and Mathematics
               </h2>
-              <p className="text-grey-text text-xl text-justify">
+              <p className="text-secondary-text text-xl text-justify">
                 My BSc studies in Mathematics included modules related to five
                 different departments of mathematics. Algebra Number Theory and
                 Mathematical Logic, Geometry, Statistics and Operational
@@ -73,21 +73,23 @@ export default class About extends React.Component {
               </p>
             </div>
             <div className="flex flex-col mt-5 flex-initial md:ml-5 md:mt-0">
-              <h2 className="text-2xl text-black-text">Skills</h2>
-              <hr className="block h-line bg-secondary w-32" />
+              <h2 className="text-2xl text-main-text">Skills</h2>
+              <hr className="block h-line bg-green w-32" />
               <ul className="pt-3 flex flex-col justify-between flex-wrap md:block">
-                <li className="text-grey-text text-xl mb-0">React</li>
-                <li className="text-grey-text text-xl mb-0">React Native</li>
-                <li className="text-grey-text text-xl mb-0">Redux</li>
-                <li className="text-grey-text text-xl mb-0">Typescript</li>
-                <li className="text-grey-text text-xl mb-0">Gatsby</li>
-                <li className="text-grey-text text-xl mb-0">Next</li>
-                <li className="text-grey-text text-xl mb-0">GraphQL</li>
-                <li className="text-grey-text text-xl mb-0">CircleCI</li>
-                <li className="text-grey-text text-xl mb-0">Node</li>
-                <li className="text-grey-text text-xl mb-0">Jest</li>
-                <li className="text-grey-text text-xl mb-0">Wordpress</li>
-                <li className="text-grey-text text-xl mb-0">Storybook</li>
+                <li className="text-secondary-text text-xl mb-0">React</li>
+                <li className="text-secondary-text text-xl mb-0">
+                  React Native
+                </li>
+                <li className="text-secondary-text text-xl mb-0">Redux</li>
+                <li className="text-secondary-text text-xl mb-0">Typescript</li>
+                <li className="text-secondary-text text-xl mb-0">Gatsby</li>
+                <li className="text-secondary-text text-xl mb-0">Next</li>
+                <li className="text-secondary-text text-xl mb-0">GraphQL</li>
+                <li className="text-secondary-text text-xl mb-0">CircleCI</li>
+                <li className="text-secondary-text text-xl mb-0">Node</li>
+                <li className="text-secondary-text text-xl mb-0">Jest</li>
+                <li className="text-secondary-text text-xl mb-0">Wordpress</li>
+                <li className="text-secondary-text text-xl mb-0">Storybook</li>
               </ul>
             </div>
           </div>
